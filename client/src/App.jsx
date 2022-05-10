80d2f9a9656634078e66bc5105f3b785ef4bb7db
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-05-11 03:29:44 +0530

    add homepage and styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,16 @@
-import React, { useContext, useEffect, useState } from 'react'
-import AddCandidate from './components/AddCandidate'
-import CandidateList from './components/CandidateList'
-import { ContractContext } from './context/ContractContext'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import HomePage from './pages/Home'
 
 const App = () => {
-  const { electionContract } = useContext(ContractContext)
-
-  const [adminAddress, setAdminAddress] = useState('')
-
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const response = await electionContract.methods.getAdmin().call()
-        setAdminAddress(response)
-      } catch (err) {
-        console.error(err)
-      }
-    }
-
-    init()
-  }, [electionContract])
-
-  if (electionContract === null) {
-    return <div>Loagin</div>
-  }
-
   return (
-    <div>
-      <h2>Admin Address : {adminAddress}</h2>
-      <CandidateList />
-      <AddCandidate />
-    </div>
+    <>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
+    </>
   )
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+
+import './bootstrap.min.css'
 import './index.css'
 import App from './App'
 
 import { ContractProvider } from './context/ContractContext'
 
 ReactDOM.render(
-  <ContractProvider>
-    <App />
-  </ContractProvider>,
+  <BrowserRouter>
+    <ContractProvider>
+      <App />
+    </ContractProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
