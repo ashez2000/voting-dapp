ed1c5e116508fb747f9bec5bab8fff22a5c7afb9
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-05-11 05:18:56 +0530

    add election context

---
+++ client/src/context/ElectionContext.jsx
@@ -0,0 +1,32 @@
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import { ContractContext } from './ContractContext'
+
+export const ElectionContext = createContext({})
+
+export const ElectionProvider = (props) => {
+  const { electionContract } = useContext(ContractContext)
+
+  const getCandidateList = async () => {
+    let error = null
+    let response = []
+
+    try {
+      // fetching candidates lt
+      response = await electionContract.methods.getCandidates().call()
+    } catch (err) {
+      error = err
+    }
+
+    return { candidateList: response, error }
+  }
+
+  const value = {
+    candidateList,
+  }
+
+  return (
+    <ElectionContext.Provider value={value}>
+      {props.children}
+    </ElectionContext.Provider>
+  )
+}
