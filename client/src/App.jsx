1b5ed235507d25c093181f6a16aa2e25edf9513d
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-05-11 03:53:33 +0530

    add admin page bioler

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 
 import Navbar from './components/Navbar'
+import AdminPage from './pages/Admin'
 import HomePage from './pages/Home'
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <div className="container">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </div>
     </>
---
+++ client/src/pages/Admin.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import AddCandidate from '../components/AddCandidate'
+import CandidateList from '../components/CandidateList'
+
+const AdminPage = () => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-6">
+          <AddCandidate />
+          <CandidateList />
+        </div>
+        <div className="col-6">
+          <div class="alert alert-warning" role="alert">
+            Status About the Election: <strong>"INIT", "LIVE, "OVER"</strong>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AdminPage
