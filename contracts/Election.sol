e6d7b955d7090abc262d0ff8aa57bf78834a531a
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-05-12 13:28:50 +0530

    add start and end func for election sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -4,10 +4,16 @@ pragma solidity ^0.8.13;
 contract Election {
   address private admin;
   uint private candidateCount;
+  bool private start;
+  bool private end;
+  enum status {INIT, LIVE, OVER} status private electionStatus;
 
   constructor() {
     admin = msg.sender;
     candidateCount = 0;
+    electionStatus = status.INIT;
+    start = false;
+    end = false;
   }
 
   modifier onlyAdmin() {
@@ -36,6 +42,8 @@ contract Election {
   }
 
   function addCandidate(string memory _name, address _ethAdd) public onlyAdmin {
+    require(electionStatus == status.INIT);
+
     Candidate memory c = Candidate({ name: _name, ethAdd: _ethAdd, uuid: candidateCount, voteCount: 0 });
     candidateMap[candidateCount] = c;
     candidateList.push(c);
@@ -43,6 +51,8 @@ contract Election {
   }
 
   function addVoters(address[] memory voters) public onlyAdmin {
+    require(electionStatus == status.INIT);
+
     for(uint i = 0; i < voters.length; i++) {
       Voter memory v = Voter({ uid: voters[i], voted: false });
       voterList[voters[i]] = v;
@@ -54,10 +64,31 @@ contract Election {
   }
 
   function vote(uint id) public {
+    require(electionStatus == status.LIVE);
+
     require(voterList[msg.sender].uid != address(0));
     require(voterList[msg.sender].voted == false);
 
     candidateList[id].voteCount++;
     voterList[msg.sender].voted = true;
+  }
+
+  function startElection() public onlyAdmin {
+    require(start == false);
+
+    electionStatus = status.LIVE;
+    start = true;
+  }
+
+  function endElection() public onlyAdmin {
+    require(start == true);
+    require(end == false);
+
+    electionStatus = status.OVER;
+    end = true;
+  }
+
+  function getElectionStatus() public view returns(status) {
+    return electionStatus;
   }
 }
