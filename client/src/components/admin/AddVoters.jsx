dc2812e5d6e7321f01976b22f62334d4f587170f
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-05-26 06:22:51 +0530

    fetch voters from api

--- client/src/components/admin/AddVoters.jsx
+++ client/src/components/admin/AddVoters.jsx
@@ -1,17 +1,29 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
+import axios from 'axios'
 import { ContractContext } from '../../context/ContractContext'
 
-import voters from '../../data/voters'
+import { API_URL } from '../../const'
 
 const AddVoters = () => {
   const { electionContract, web3 } = useContext(ContractContext)
+  const [userList, setUserList] = useState([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const res = await axios.get(`${API_URL}/auth/users`)
+      setUserList(res.data.users)
+    }
+
+    fetchUsers()
+  }, [])
 
   const addVoters = async () => {
+    const users = userList.map((u) => u.ethAccount)
+
     try {
       const accounts = await web3.eth.getAccounts()
-
       await electionContract.methods
-        .addVoters(voters)
+        .addVoters(users)
         .send({ from: accounts[0] })
       alert('Voters added successfully')
     } catch (err) {
@@ -21,11 +33,31 @@ const AddVoters = () => {
   }
 
   return (
-    <div>
-      <button className="btn btn-light btn-lg" onClick={addVoters}>
-        Import Voters
-      </button>
-    </div>
+    <>
+      <div className="d-flex justify-content-center">
+        <button className="btn btn-primary" onClick={addVoters}>
+          Import Voters
+        </button>
+      </div>
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userList.map((c) => (
+              <tr key={c._id}>
+                <td>{c.username}</td>
+                <td>{c.email}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
