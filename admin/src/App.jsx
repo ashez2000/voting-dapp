f38ada17ca8bc5a5602fb7617a48568f780426e2
Aswin Gangadharan <aswin.gangadharan.2020@gmail.com>
2022-06-24 01:34:21 +0530

    clean admin boiler

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,10 @@
 const App = () => {
-  return <div>App</div>
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button className="btn btn-primary">Login</button>
+    </div>
+  )
 }
 
 export default App
